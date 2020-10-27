21843fb5c1ba9e5365522a43372ae2317fb9e29f
nverno <noah.v.peart@gmail.com>
2020-10-27 18:45:04 -0400

    Add ar lite part II- associations

--- W6D2/active-record-lite/lib/03_associatable.rb
+++ W6D2/active-record-lite/lib/03_associatable.rb
@@ -10,41 +10,60 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @class_name = options[:class_name] || name.to_s.camelcase
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      fkey = send(options.foreign_key)
+      options.model_class.where(options.primary_key => fkey.to_s).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      pkey = send(options.primary_key)
+      options.model_class.where(options.foreign_key => pkey.to_s)
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
--- W6D2/active-record-lite/lib/04_associatable2.rb
+++ W6D2/active-record-lite/lib/04_associatable2.rb
@@ -5,6 +5,29 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    t_opts = assoc_options[through_name]
+
+    # SELECT
+    #   houses.*
+    # FROM
+    #   humans
+    # JOIN
+    #   houses ON humans.house_id = houses.id
+    # WHERE
+    #   humans.id = ?
+    define_method(name) do
+      s_opts = t_opts.model_class.assoc_options[source_name]
+      s_table = s_opts.table_name
+      t_table = t_opts.table_name
+      val = send(t_opts.foreign_key)
+      res = DBConnection.execute(<<-SQL, val)
+        SELECT #{s_table}.*
+        FROM #{t_table}
+        JOIN #{s_table}
+        ON #{t_table}.#{s_opts.foreign_key} = #{s_table}.#{s_opts.primary_key}
+        WHERE #{t_table}.#{t_opts.primary_key} = ?
+      SQL
+      s_opts.model_class.parse_all(res).first
+    end
   end
 end
